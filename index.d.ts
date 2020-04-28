0069e7e5db172cb6683616c9134f29acf86ad6bd
Rudi Chen <rudichen@gmail.com>
2020-04-27 17:22:41 -0700

    Version 1, Update 10

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,4 @@
-// Figma Plugin API version 1, update 9
+// Figma Plugin API version 1, update 10
 
 declare global {
   // Global variable with Figma's plugin API.
@@ -533,7 +533,7 @@ declare global {
   }
 
   interface ReactionMixin {
-    readonly reactions: ReadonlyArray<Reaction> // PROPOSED API ONLY
+    readonly reactions: ReadonlyArray<Reaction>
   }
 
   interface DefaultShapeMixin extends
@@ -553,12 +553,12 @@ declare global {
     verticalPadding: number // applicable only if layoutMode != "NONE"
     itemSpacing: number // applicable only if layoutMode != "NONE"
 
-    overflowDirection: OverflowDirection // PROPOSED API ONLY
-    numberOfFixedChildren: number // PROPOSED API ONLY
+    overflowDirection: OverflowDirection
+    numberOfFixedChildren: number
 
-    readonly overlayPositionType: OverlayPositionType // PROPOSED API ONLY
-    readonly overlayBackground: OverlayBackground // PROPOSED API ONLY
-    readonly overlayBackgroundInteraction: OverlayBackgroundInteraction // PROPOSED API ONLY
+    readonly overlayPositionType: OverlayPositionType
+    readonly overlayBackground: OverlayBackground
+    readonly overlayBackgroundInteraction: OverlayBackgroundInteraction
   }
 
   ////////////////////////////////////////////////////////////////////////////////
@@ -585,7 +585,7 @@ declare global {
 
     backgrounds: ReadonlyArray<Paint>
 
-    readonly prototypeStartNode: FrameNode | GroupNode | ComponentNode | InstanceNode | null // PROPOSED API ONLY
+    readonly prototypeStartNode: FrameNode | GroupNode | ComponentNode | InstanceNode | null
   }
 
   interface FrameNode extends DefaultFrameMixin {
