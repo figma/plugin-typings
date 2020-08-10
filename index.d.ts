7df79aac5135bc742594c142cf19ce5cef4faa44
Rudi Chen <rudichen@gmail.com>
2020-08-10 12:42:29 -0700

    Fixup mainComponent

--- index.d.ts
+++ index.d.ts
@@ -757,7 +757,7 @@ declare global {
   interface InstanceNode extends DefaultFrameMixin  {
     readonly type: "INSTANCE"
     clone(): InstanceNode
-    masterComponent: ComponentNode
+    mainComponent: ComponentNode
     scaleFactor: number
   }
 
