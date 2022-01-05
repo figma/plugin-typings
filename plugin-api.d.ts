1cb1438f64765248296801e9cfe1d5723e63d44c
Karl Jiang <karl@figma.com>
2022-01-05 08:16:08 -1000

    change fillStyleId to string

--- plugin-api.d.ts
+++ plugin-api.d.ts
@@ -451,7 +451,7 @@ interface VectorRegion {
   readonly windingRule: WindingRule
   readonly loops: ReadonlyArray<ReadonlyArray<number>>
   readonly fills?: ReadonlyArray<Paint>
-  readonly fillStyleId?: { key: string, version: string } | null
+  readonly fillStyleId?: string | null
 }
 
 interface VectorNetwork {
