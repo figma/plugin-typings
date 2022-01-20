7fb5a297f57df220ef6588861818d23c8cc469e2
Ben Smith <bsmith@figma.com>
2022-01-20 12:26:06 -0800

    Add getFileThumbnailNode and setFileThumbnailNodeAsync() (#111)

--- plugin-api.d.ts
+++ plugin-api.d.ts
@@ -122,6 +122,9 @@ interface PluginAPI {
 
   base64Encode(data: Uint8Array): string
   base64Decode(data: string): Uint8Array
+
+  getFileThumbnailNode(): FrameNode | ComponentNode | ComponentSetNode | null
+  setFileThumbnailNodeAsync(node: FrameNode | ComponentNode | ComponentSetNode | null): Promise<void>
 }
 
 interface ClientStorageAPI {
