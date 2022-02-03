fbde7576d13def79088d48ba0dce172c0cf86943
Ben Smith <bsmith@figma.com>
2022-02-03 12:06:51 -0800

    Remove ;

--- index.d.ts
+++ index.d.ts
@@ -3,52 +3,52 @@
 
 declare global {
   // Global variable with Figma's plugin API.
-  const figma: PluginAPI;
-  const __html__: string;
+  const figma: PluginAPI
+  const __html__: string
   const __uiFiles__: {
-    [key: string]: string;
-  };
-  const console: Console;
+    [key: string]: string
+  }
+  const console: Console
 
   // The plugin environment exposes the browser console API,
   // so expected calls like console.log() still work.
   interface Console {
-    log(message?: any, ...optionalParams: any[]): void;
-    error(message?: any, ...optionalParams: any[]): void;
-    assert(condition?: boolean, message?: string, ...data: any[]): void;
-    info(message?: any, ...optionalParams: any[]): void;
-    warn(message?: any, ...optionalParams: any[]): void;
-    clear(): void;
+    log(message?: any, ...optionalParams: any[]): void
+    error(message?: any, ...optionalParams: any[]): void
+    assert(condition?: boolean, message?: string, ...data: any[]): void
+    info(message?: any, ...optionalParams: any[]): void
+    warn(message?: any, ...optionalParams: any[]): void
+    clear(): void
   }
-  function setTimeout(callback: Function, timeout: number): number;
-  function clearTimeout(handle: number): void;
-  function setInterval(callback: Function, timeout: number): number;
-  function clearInterval(handle: number): void;
+  function setTimeout(callback: Function, timeout: number): number
+  function clearTimeout(handle: number): void
+  function setInterval(callback: Function, timeout: number): number
+  function clearInterval(handle: number): void
 
-  const fetch: (url: string, init?: FetchOptions) => Promise<FetchResponse>;
+  const fetch: (url: string, init?: FetchOptions) => Promise<FetchResponse>
 
   interface FetchOptions {
-    method?: string;
-    headersObject?: { [name: string]: string };
-    body?: Uint8Array;
-    credentials?: string;
-    cache?: string;
-    redirect?: string;
-    referrer?: string;
-    integrity?: string;
+    method?: string
+    headersObject?: { [name: string]: string }
+    body?: Uint8Array
+    credentials?: string
+    cache?: string
+    redirect?: string
+    referrer?: string
+    integrity?: string
   }
 
   interface FetchResponse {
-    headersObject: { [name: string]: string };
-    ok: boolean;
-    redirected: boolean;
-    status: number;
-    statusText: string;
-    type: string;
-    url: string;
-    arrayBuffer(): Promise<ArrayBuffer>;
-    text(): Promise<string>;
+    headersObject: { [name: string]: string }
+    ok: boolean
+    redirected: boolean
+    status: number
+    statusText: string
+    type: string
+    url: string
+    arrayBuffer(): Promise<ArrayBuffer>
+    text(): Promise<string>
   }
 } // declare global
 
-export {};
+export {}
