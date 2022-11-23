fd96ecf1b09a527177b1194d0150cae1d7432468
Sawyer Hood <sawyerjhood@gmail.com>
2022-11-23 13:06:24 -0800

    Allow text review to return synchronously

--- plugin-api.d.ts
+++ plugin-api.d.ts
@@ -54,19 +54,28 @@ interface PluginAPI {
   on(type: 'run', callback: (event: RunEvent) => void): void
   on(type: 'drop', callback: (event: DropEvent) => boolean): void
   on(type: 'documentchange', callback: (event: DocumentChangeEvent) => void): void
-  on(type: 'textreview', callback: (event: TextReviewEvent) => Promise<TextReviewRange[]>): void
+  on(
+    type: 'textreview',
+    callback: (event: TextReviewEvent) => Promise<TextReviewRange[]> | TextReviewRange[],
+  ): void
 
   once(type: ArgFreeEventType, callback: () => void): void
   once(type: 'run', callback: (event: RunEvent) => void): void
   once(type: 'drop', callback: (event: DropEvent) => boolean): void
   once(type: 'documentchange', callback: (event: DocumentChangeEvent) => void): void
-  once(type: 'textreview', callback: (event: TextReviewEvent) => Promise<TextReviewRange[]>): void
+  once(
+    type: 'textreview',
+    callback: (event: TextReviewEvent) => Promise<TextReviewRange[]> | TextReviewRange[],
+  ): void
 
   off(type: ArgFreeEventType, callback: () => void): void
   off(type: 'run', callback: (event: RunEvent) => void): void
   off(type: 'drop', callback: (event: DropEvent) => boolean): void
   off(type: 'documentchange', callback: (event: DocumentChangeEvent) => void): void
-  off(type: 'textreview', callback: (event: TextReviewEvent) => Promise<TextReviewRange[]>): void
+  off(
+    type: 'textreview',
+    callback: (event: TextReviewEvent) => Promise<TextReviewRange[]> | TextReviewRange[],
+  ): void
 
   readonly mixed: unique symbol
 
