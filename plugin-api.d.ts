7d735873db2bbc57c2b17dd6322477aad25557cd
Elaine Lin <elaine.lin@figma.com>
2023-04-04 18:02:25 -0700

    Revert "Merge pull request #203 from figma/joanna/update-typings-textbundle"
    This reverts commit 88654a4e19a09a71668575402c54ec761d834591, reversing
    changes made to 14f5f5caf94ea9eb826474ac215de076eb4d3a8b.

--- plugin-api.d.ts
+++ plugin-api.d.ts
@@ -356,12 +356,8 @@ declare type NodeChangeProperty =
   | 'innerRadius'
   | 'fontSize'
   | 'lineHeight'
-  | 'leadingTrim'
   | 'paragraphIndent'
   | 'paragraphSpacing'
-  | 'listSpacing'
-  | 'hangingPunctuation'
-  | 'hangingList'
   | 'letterSpacing'
   | 'textAlignHorizontal'
   | 'textAlignVertical'
@@ -458,12 +454,8 @@ declare type StyleChangeProperty =
   | 'textDecoration'
   | 'letterSpacing'
   | 'lineHeight'
-  | 'leadingTrim'
   | 'paragraphIndent'
   | 'paragraphSpacing'
-  | 'listSpacing'
-  | 'hangingPunctuation'
-  | 'hangingList'
   | 'textCase'
   | 'paint'
   | 'effects'
@@ -690,7 +682,6 @@ declare type LineHeight =
   | {
       readonly unit: 'AUTO'
     }
-declare type LeadingTrim = 'CAP_HEIGHT' | 'NONE'
 declare type HyperlinkTarget = {
   type: 'URL' | 'NODE'
   value: string
@@ -1130,9 +1121,6 @@ interface TextSublayerNode extends MinimalFillsMixin {
   readonly hasMissingFont: boolean
   paragraphIndent: number
   paragraphSpacing: number
-  listSpacing: number
-  hangingPunctuation: boolean
-  hangingList: boolean
   fontSize: number | PluginAPI['mixed']
   fontName: FontName | PluginAPI['mixed']
   readonly fontWeight: number | PluginAPI['mixed']
@@ -1140,7 +1128,6 @@ interface TextSublayerNode extends MinimalFillsMixin {
   textDecoration: TextDecoration | PluginAPI['mixed']
   letterSpacing: LetterSpacing | PluginAPI['mixed']
   lineHeight: LineHeight | PluginAPI['mixed']
-  leadingTrim: LeadingTrim | PluginAPI['mixed']
   hyperlink: HyperlinkTarget | null | PluginAPI['mixed']
   characters: string
   insertCharacters(start: number, characters: string, useStyle?: 'BEFORE' | 'AFTER'): void
@@ -1592,12 +1579,8 @@ interface TextStyle extends BaseStyle {
   fontName: FontName
   letterSpacing: LetterSpacing
   lineHeight: LineHeight
-  leadingTrim: LeadingTrim
   paragraphIndent: number
   paragraphSpacing: number
-  listSpacing: number
-  hangingPunctuation: boolean
-  hangingList: boolean
   textCase: TextCase
 }
 interface EffectStyle extends BaseStyle {
