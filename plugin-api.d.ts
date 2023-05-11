a1d7ac8c98d5104d07302f9386ef2ae1e2f90854
Rohit Chouhan <ro.chouhan@gmail.com>
2023-05-11 12:38:21 -0500

    Add options to getRelatedLinksAsync

--- plugin-api.d.ts
+++ plugin-api.d.ts
@@ -1021,7 +1021,7 @@ interface BaseNodeMixin extends PluginDataMixin {
     [command: string]: string
   }
   getCSSAsync(): Promise<{ [key: string]: string }>
-  getRelatedLinksAsync(): Promise<RelatedLink[]>
+  getRelatedLinksAsync(options?: {includeChildren?: boolean}): Promise<RelatedLinkWithNodeId[]>
   addRelatedLinkAsync(url: string, name?: string): Promise<void>
   editRelatedLinkAsync(currentUrl: string, newValue: { name?: string; url?: string }): Promise<void>
   deleteRelatedLinkAsync(url: string): Promise<void>
